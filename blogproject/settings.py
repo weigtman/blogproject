91f5e849988f04054b890c9a5a5adef3cbb98951
wj.zhai <wj.zhai@signway.cn>
2019-03-17 17:21:22 +0800

    #!/usr/bin/python #coding:utf-8

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vh&g2ab0+n6v4v3sa66!a=w6ig#tud_fz7opojh1v$2rsjj_&)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.weigtman.top']
 
